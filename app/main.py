305c4a4d2f8751d21279c8cf11da0a792c1370e7
lucasrso <lucasrso2005@gmail.com>
2024-06-19 20:53:36 -0300

    Modificando a estrutura de pastas e iniciando o MOUNT dos arquivos

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text  
-from .database import engine, Base, get_db
-import app.routers.pc as pc
+from database import engine, Base, get_db
+import routers.pc as pc
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+import uvicorn
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind= engine)
 
 app = FastAPI()
 
@@ -13,9 +18,51 @@ app.include_router(pc.router)
 @app.get("/healthcheck")
 def healthcheck(db: Session = Depends(get_db)):
     try:
-       
         db.execute(text("SELECT 1"))  
         return {"status": "ok", "message": "Conectado na base de dados"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
     
+# Corrected mount path for static files
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+# Corrected path for templates
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        name='index.html',
+        context={'request': request}
+    )
+
+@app.get('/criar_conta', response_class=HTMLResponse)
+def criar_conta(request: Request):
+    return templates.TemplateResponse(
+        name='criar_conta.html',
+        context={'request': request}
+    )
+
+@app.get('/delete_item', response_class=HTMLResponse)
+def delete_item(request: Request):
+    return templates.TemplateResponse(
+        name='delete_item.html',
+        context={'request': request}
+    )
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        name='index.html',
+        context={'request': request}
+    )
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        name='index.html',
+        context={'request': request}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(app)
